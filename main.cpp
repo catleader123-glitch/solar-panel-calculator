0c2049ee77244a403d42885e0c4a1a07a1329a21
catleader123-glitch <catleader123@gmail.com>
2026-01-20 15:10:49 -0700

    Update main.cpp
    somthing

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
-//worms
+#include <iostream>
+using namespace std;
+
+int panelwattpci = 1; //placeholder value
+int sunpos = 180; //90 is sunrise 270 is sunset
+int pannelrot = 125; //the rot for the panel 0 is facing down
+int pannelface = 2; //1 is north 2 is east 3 is south 4 is west
+
+void pm() { //math
+
+}
+
+void pmoutput() { //print for cleaner scripts for me
+
+}
+
+main(){
+    cout << "worms"; //worms is a hello world for me
+
+}
